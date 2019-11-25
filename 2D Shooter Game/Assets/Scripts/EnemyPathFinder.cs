1025b9858eebdf7cd58dd7f66d6a826a3f5faed0
EParrish19 <ethan.parrish@students.plymouth.ac.uk>
2019-11-25 16:58:50 +0000

    Added Pathfinding
    used A* to add pathfinding to the zombies.

---
+++ 2D Shooter Game/Assets/Scripts/EnemyPathFinder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyPathFinder : MonoBehaviour
+{
+    public Transform target;
+    private IAstarAI ai;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ai = GetComponent<IAstarAI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target !=null && ai != null)
+        {
+            ai.destination = target.position;
+            ai.SearchPath();
+        }
+    }
+
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+}
