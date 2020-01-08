3d00eafa72b8481b000017bea4fe00b9fc7af562
EParrish19 <ethan.parrish@students.plymouth.ac.uk>
2020-01-08 18:23:33 +0000

    Sounds and Final Tweaks
    added sounds to the game and made some final edits to the game.

---
+++ 2D Shooter Game/Assets/Scripts/StartText.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartText : MonoBehaviour
+{
+    public Text BeginText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BeginText = GetComponent<Text>();
+        var tempcolor = BeginText.color;
+        tempcolor.a = 0.1f;
+        BeginText.color = tempcolor;
+        BeginText.CrossFadeAlpha(255f, 3f, false);
+        FadeOut();
+    }
+
+    // Update is called once per frame
+    void FadeOut()
+    {
+        BeginText.CrossFadeAlpha(0f, 3f, false);
+    }
+}
